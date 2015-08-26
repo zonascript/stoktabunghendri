efe8520df70a8f4ecb8c44558ae2162224a28b47
blackbone23 <rully.lukman@gmail.com>
2015-08-27 01:04:11 +0800

    tabung yang mau expired terlihat di halaman website

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	include "header.php";
 //echo $_SESSION['username'];
 //echo $_SESSION['permission'];
+@$transaksis= getTransaksi();
 ?>
 
 <div class="container">
@@ -14,6 +15,65 @@
 		echo "halo";
 	?>
     </p>
+    
+    <div id="tabung-expired">
+    <p class="lead">Daftar Tabung Yang Mau Expired</p>
+	<table border="1" cellpadding="10" cellspacing="0">
+	  <tr>
+	    <th>Nomor PO</th>
+	    <th>Nomor Seri</th>
+	    <th>Tanggal Keluar</th>
+	    <th>Tanggal Kembali</th>
+	    <th>Nama Customer</th>
+	    <th>Alamat</th>
+	    <th>Status</th>
+	    <th>Expired</th>
+	  </tr>
+	
+	 <?php 
+	 $values = getMasaAktifTabung();
+	 $day = $values[0]['value'];
+	 //echo $day;
+	 $yellow = $day - ($day/3);
+	 //echo $yellow;
+	foreach($transaksis as $row)
+	{
+		$now=time();
+		$selling_date = strtotime($row['tgl_keluar']);
+		$datediff = $now - $selling_date;
+		$days = floor($datediff/(60*60*24));
+	//	var_dump($row);
+		if($days>=$yellow && $days<$day-1 && $row['status']=='terjual'){
+			echo "<tr>";
+			echo "<td>".$row['no_po']."</td>";
+			echo "<td>".$row['no_seri']."</td>";
+			echo "<td>".$row['tgl_keluar']."</td>";
+			echo "<td>".$row['tgl_kembali']."</td>";
+			echo "<td>".$row['nama_cust']."</td>";
+			echo "<td>".$row['alamat']."</td>";
+			echo "<td>".$row['status']."</td>";
+			echo "<td style='background:yellow'>Hampir Expired</td>";
+		} elseif ($days>=$day-1 && $row['status']=='terjual'){
+			echo "<tr>";
+			echo "<td>".$row['no_po']."</td>";
+			echo "<td>".$row['no_seri']."</td>";
+			echo "<td>".$row['tgl_keluar']."</td>";
+			echo "<td>".$row['tgl_kembali']."</td>";
+			echo "<td>".$row['nama_cust']."</td>";
+			echo "<td>".$row['alamat']."</td>";
+			echo "<td>".$row['status']."</td>";
+			echo "<td style='background:red'>Sudah Expired</td>";
+		} else {
+			
+		}
+		echo "</tr>";
+		
+	}
+	
+	?>
+	</table>
+    </div>
+    
   </div>
   
 </div><!-- /.container -->

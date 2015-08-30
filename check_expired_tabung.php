78e808ebdb711e96da4388d42474ec5dca551172
blackbone23 <rully.lukman@gmail.com>
2015-08-31 01:21:27 +0800

    merge

--- check_expired_tabung.php
+++ check_expired_tabung.php
@@ -13,7 +13,7 @@ foreach ($transaksis as $row):
 	$yellow = $day - ($day/3);
 	$now=time();
 	$selling_date = strtotime($row['tgl_keluar']);
-	//$selling_date = strtotime('2015-08-29');
+	//$selling_date = strtotime('2015-06-29');
 	$datediff = $now - $selling_date;
 	$days = floor($datediff/(60*60*24));
 	$remainingdays = $day - $days;
@@ -43,10 +43,10 @@ endforeach;
 
 if ($send_email==true):
 	$email_target = "rully.lukman@gmail.com";
-	$user_sender = "Reminder Tabung Oksigen";
+	$user_sender = "From : rully.lukman@gmail.com";
 	$email_subject = "Warning, Kami Mendeteksi Ada Tabung Yang Hampir Kadaluarsa";
 	$email_message =  "Hi Hendri,\n\nBerikut adalah daftar tabung yang harus segera ditarik, karena sudah masuk masa kadaluarsa (expired).\n\n".$content."\n\nTerima kasih atas perhatiannya.\n\nSalam,\n\n\nAdministrator";
-	mail($email_target,$email_subject,$email_message,"From : ".$user_sender);
+	mail($email_target,$email_subject,$email_message,$user_sender);
 endif;
 
 ?>

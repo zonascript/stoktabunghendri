edaba2975d1aea8402563722638913c8dad6238e
blackbone23 <rully.lukman@gmail.com>
2015-09-14 02:03:07 +0800

    edit kategori3

--- edit_kategori.php
+++ edit_kategori.php
@@ -5,4 +5,32 @@ $id_kategori = $_GET["id_kategori"];
 
 echo $id_kategori;
 
+@$kategoris = getDataKategori($id_kategori);
+
+//var_dump($barangs);
 ?>
+
+<div class="container">
+  
+  <div class="text-center">
+  
+	<form method="post" name="edit_kategori" action="kategori_edit.php">
+	<?php 
+	foreach ($kategoris	as $row):
+	?>
+	<input type="hidden" name="id_kategori" value="<?php echo $row['id_kategori'];?>"/>
+	
+	<table>
+		  <tr>
+		  	<td><span>Nama Kategori : </span></td>
+		  	<td><input type="text" name="nama_kategori" value="<?php echo $row['nama_kategori']?>"/></td>
+		  </tr>
+		</tr>
+	</table>
+	<input type="submit" name="submit" value="Edit Data Kategori"/>
+	</form>
+
+  </div>
+  
+</div><!-- /.container -->
+<?php include 'footer.php';?>

23344817fb2de56924f76deaa173803c60a3c06a
blackbone23 <rully.lukman@gmail.com>
2015-09-14 01:09:50 +0800

    lihat kategori dan input digabung

--- kategori.php
+++ kategori.php
@@ -51,6 +51,19 @@ include 'header.php';
 	</div>
 	</div>
 	
+	<h2>Input Kategori</h2>
+	<p><span class="error">* harus diisi</span></p>
+	<form name="input_kategori" method="post" action="kategori_input.php">
+	<table>
+		  <tr>
+		  	<td><span>Nama Kategori : </span></td>
+		  	<td><input type="text" name="nama_kategori"/><span class="error"> * <?php echo $codeErr;?></span></td>
+		  </tr>
+	</table>
+	<br/>
+	<input type="submit" name="submit" value="Submit"> 
+	</form>
+	
 	
 	
   </div>

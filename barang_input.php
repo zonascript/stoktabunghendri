848f1ae4e0da5580d23c792c362d5dc347b47d0c
blackbone23 <rully.lukman@gmail.com>
2015-09-14 02:57:10 +0800

    add kategori barang

--- barang_input.php
+++ barang_input.php
@@ -7,8 +7,8 @@ $harga_dasar= $_POST['harga_dasar'];
 $harga_jual= $_POST['harga_jual'];
 $status = "Available";
 $kategori = $_POST['kategori'];
-
-$a= input_barang($no_seri, $no_ketok, $harga_dasar, $harga_jual, $status, $kategori);
+echo $kategori;
+//$a= input_barang($no_seri, $no_ketok, $harga_dasar, $harga_jual, $status, $kategori);
 
 if (isset($a)){
 	header("Location: $base_url/barang.php");

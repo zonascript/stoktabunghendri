6c4bd45c07d45b627af5686e77273f1729aa975c
blackbone23 <rully.lukman@gmail.com>
2015-09-16 03:37:59 +0800

    edit penjualan

--- penjualan_input.php
+++ penjualan_input.php
@@ -90,21 +90,20 @@ $harga_jual_faktur = $barang[0]['harga_jual'];
 				foreach ($kategoris as $row):
 					$row_kategori = $row['nama_kategori'];
 					$jumlah_per_kategoris = getJumlahKategoriTransaksi($po_fak,$row_kategori);
-					//$jumlah_per_kategori = $jumlah_per_kategoris[0]['COUNT(*)']);
-					//if($jumlah_per_kategori != "0"):
-						echo "halo";
+					$jumlah_per_kategori = $jumlah_per_kategoris[0]['COUNT(*)'];
+					if($jumlah_per_kategori != "0"):
 			?>	
-				
-			<?php
-					//endif;
-				endforeach;
-			  ?>
 			  <tr>
-			  	<td>Tabung Oksigen</td>
-			    <td style="border-right:1px solid black;"><?php echo $jumlah_tabung;?></td>
+			  	<td><?php $nama_kategori; ?></td>
+			    <td style="border-right:1px solid black;"><?php echo $jumlah_per_kategori;?></td>
 			    <td style="border-right:1px solid black;"><?php echo $harga_jual_faktur;?></td>
 			    <td><?php echo $jumlah_tabung * $harga_jual_faktur;?></td>
-			  </tr>
+			  </tr>				
+			<?php
+					endif;
+				endforeach;
+			  ?>
+
 			</table>
 		</div>
 		<div class="ttd-author">

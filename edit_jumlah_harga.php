ee4dfc93d2584a82a6a92280d732b6507bf22897
blackbone23 <rully.lukman@gmail.com>
2015-09-16 11:45:10 +0800

    create harga khusus

--- edit_jumlah_harga.php
+++ edit_jumlah_harga.php
@@ -2,9 +2,9 @@
 include 'db/pdo.php';
 
 $harga_faktur = $_GET['harga_faktur'];
-$jumlah_tabung = $_GET['jumlah_tabung'];
+//$jumlah_tabung = $_GET['jumlah_tabung'];
 
 //$barangs = getBarangAvail();
 //$customers = getCustomer();
-echo $harga_faktur * $jumlah_tabung;
+echo $harga_faktur;
 ?>

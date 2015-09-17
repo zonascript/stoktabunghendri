437ff1b2c57421115273fe6ed4ddb00640a11ca0
blackbone23 <rully.lukman@gmail.com>
2015-09-17 23:42:04 +0800

    edit penjualan input

--- penjualan_input.php
+++ penjualan_input.php
@@ -49,7 +49,8 @@ if($jumlah_tabung==""){
 		 	$nomor_seri_tabung = $_POST['pilih_barang_'.$i.''];
 			$kategoris = getDataKategoriBarang($nomor_seri_tabung);
 			$kategori = $kategoris[0]['kategori'];
-			var_dump($kategori);
+			$harga_jual = getHargaBarangKategori($kategori);
+			var_dump($harga_jual);
 		 	//inputTransaksi($po_fak,$nomor_seri_tabung,$kategori,$tanggal,$nama_cust,$alamat,$status);
 		 	//updateBarangKeluar($nomor_seri_tabung);
 		 }
